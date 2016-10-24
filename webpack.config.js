cd48c77666d916d1ab5166a28a8b0c3f95e6985a
Mike Boan <mikeboan@gmail.com>
2016-10-24 13:15:00 -0400

    Complete fundamentals. TODO: more styling, import live snake and battle tetris

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  context: __dirname,
+  entry: "./lib/javascript/portfolio.js",
+  output: {
+    path: "./lib",
+    publicPath: "/lib/",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  devtool: 'source-map'
+};
